2de091736ac9b02b778900b99b3fb0f56979202f
sekury <sergey.kuryugin@ya.ru>
2022-03-15 16:06:10 +0300

    Fix validation

--- src/main/java/assignment/proxyservice/controller/ProxyController.java
+++ src/main/java/assignment/proxyservice/controller/ProxyController.java
@@ -5,12 +5,16 @@ import assignment.proxyservice.model.ProxyDto;
 import assignment.proxyservice.service.ProxyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/proxies")
 public class ProxyController {
@@ -32,8 +36,8 @@ public class ProxyController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE
     )
     public List<ProxyDto> getAllProxies(
-            @RequestParam(name = "page", defaultValue = "0") int page,
-            @RequestParam(name = "size", defaultValue = "20") int size
+            @RequestParam(name = "page", defaultValue = "0") @Min(0) int page,
+            @RequestParam(name = "size", defaultValue = "20") @Min(1) @Max(20) int size
     ) {
         return proxyService.getProxies(page, size);
     }
